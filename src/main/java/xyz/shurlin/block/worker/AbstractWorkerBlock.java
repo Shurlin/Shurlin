8a403b617993f4aefec9c89d08215cb1e67b28f6
Shurlin <jibanxianling@qq.com>
2020-10-04 21:53:10 +0800

    0.1 upload

--- src/main/java/xyz/shurlin/block/worker/AbstractWorkerBlock.java
+++ src/main/java/xyz/shurlin/block/worker/AbstractWorkerBlock.java
@@ -1,15 +1,20 @@
 package xyz.shurlin.block.worker;
 
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import xyz.shurlin.block.worker.entity.AbstractWorkerBlockEntity;
 
@@ -44,6 +49,22 @@ abstract class AbstractWorkerBlock extends BlockWithEntity {
             if (blockEntity instanceof AbstractWorkerBlockEntity) {
                 ((AbstractWorkerBlockEntity)blockEntity).setCustomName(itemStack.getName());
             }
+        }
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if (!state.isOf(newState.getBlock())) {
+            BlockEntity blockEntity = world.getBlockEntity(pos);
+            if (blockEntity instanceof AbstractWorkerBlockEntity) {
+                ItemScatterer.spawn(world, pos, (AbstractWorkerBlockEntity)blockEntity);
+                world.updateComparators(pos, this);
+            }
+            super.onStateReplaced(state, world, pos, newState, moved);
         }
     }
 }
