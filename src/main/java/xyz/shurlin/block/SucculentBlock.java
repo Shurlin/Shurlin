abee0223b9da0e81f5a4689eb5eedaef61f00fa3
Shurlin <jibanxianling@qq.com>
2020-11-14 17:51:37 +0800

    0.1 upload

--- src/main/java/xyz/shurlin/block/SucculentBlock.java
+++ src/main/java/xyz/shurlin/block/SucculentBlock.java
@@ -2,15 +2,26 @@ package xyz.shurlin.block;
 
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SlimeBlock;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import xyz.shurlin.Shurlin;
 
 public class SucculentBlock extends SlimeBlock {
     public static final BooleanProperty SHINING = BooleanProperty.of("shining");
 
     public SucculentBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.getStateManager().getDefaultState().with(SHINING, Shurlin.random.nextFloat()<0.05));
     }
 
-
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(SHINING);
+    }
 }
--- src/main/java/xyz/shurlin/reflect/Reflector.java
+++ src/main/java/xyz/shurlin/reflect/Reflector.java
@@ -5,9 +5,7 @@ import net.minecraft.entity.EntityPose;
 import java.lang.reflect.Constructor;
 
 public class Reflector {
+    public static final EnumBuster<EntityPose> ENTITY_POSE_ENUM_BUSTER = new EnumBuster<>(EntityPose.class);
+    public static final EntityPose MEDITATING = ENTITY_POSE_ENUM_BUSTER.make("MEDITATING");
 
-    public Reflector() {
-        Class entityPoseClass = EntityPose.class;
-//        Constructor constructor = entityPoseClass.getConstructor();
-    }
 }
